76df3a2ed0b28824a54bfedd47256bd4f7b5d2d2
Flaki <slsoftworks@gmail.com>
2015-01-15 11:04:09 +0100

    Transpiling output (initial)

--- transpile.js
+++ transpile.js
@@ -42,17 +42,21 @@ function traverse(object, visitor, parent) {
 var ast = esprima.parse(script),
 	astl = esprima.parse(script, { range: true });
 
-console.log(JSON.stringify(ast, null, 2));
+//console.log(JSON.stringify(ast, null, 2));
 
-script.split(/\n/).map(function(line, n) { console.log(++n +'  '+ line); });
+
+// Create output script
+var outScript = script,
+	outOffset = 0;
 
 traverse(astl, function(n, pn) {
 	if (n.type === 'Identifier' && n.name === 'dataset') {
-		console.log('.dataset @ '+n.range[0]+'-'+n.range[1]+' ('+pn.length+')');
+		//console.log('.dataset @ '+n.range[0]+'-'+n.range[1]+' ('+pn.length+')');
 		//console.log(JSON.stringify(pn));
 		//console.log('> ' + script.substring(pn[pn.length - 1].range[0], n.range[0]-1));
 
-		var dataElement, dataProperty, dataValue;
+		var dataElement, dataProperty, dataValue,
+			translateStart, translateEnd, translateFrom, translateTo;
 
 		dataElement = script.substring(pn[pn.length - 1].range[0], n.range[0]-1);
 		//console.log(pn[pn.length - 2].property);
@@ -69,9 +73,31 @@ traverse(astl, function(n, pn) {
 			dataValue = null;
 		}
 
-		console.log(script.substring(pn[pn.length - (dataValue?3:2)].range[0], pn[pn.length - (dataValue?3:2)].range[1]) + ' ->');
-		console.log('Data.'+(dataValue?'set':'get')+'( ' + dataElement + ', ' + dataProperty + (dataValue?', '+dataValue:'') + ' )');
-		console.log(pn.reduce(function(p,c) { return p +'> '+ (c.type||'?') +' '; }, '|'));
+		// Translate (splice) boundaries
+		translateStart = pn[pn.length - (dataValue ? 3 : 2)].range[0];
+		translateEnd =   pn[pn.length - (dataValue ? 3 : 2)].range[1];
+
+		translateFrom = script.substring(translateStart, translateEnd);
+		translateTo = 'Data.'
+			+ (dataValue ? 'set' : 'get')
+			+ '( ' + dataElement + ', ' + dataProperty + (dataValue ? ', ' + dataValue : '') + ' )';
+
+		//console.log(translateFrom + ' ->' +translateTo);
+		//console.log(pn.reduce(function(p,c) { return p +'> '+ (c.type||'?') +' '; }, '|'));
 		//console.log(JSON.stringify(pn[pn.length - 2],null,4));
+
+		// Splice output
+		outScript = outScript.splice(translateStart + outOffset, translateEnd + outOffset, translateTo);
+
+		// Update offset
+		outOffset += translateTo.length - (translateEnd - translateStart);
 	}
 });
+
+// Display original script
+console.log('\nOriginal script:');
+script.split(/\n/).map(function(line, n) { console.log(++n +'  '+ line); });
+
+// Display script output
+console.log('\nTranspiled output:');
+outScript.split(/\n/).map(function(line, n) { console.log(++n +'  '+ line); });
